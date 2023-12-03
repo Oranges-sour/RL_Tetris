0d8fdceb6819f212ab19f19bae0010f1f4d0d1a5
Oranges-sour <873516725@qq.com>
2023-12-03 20:45:05 +0800

    1

--- src/main_train.py
+++ src/main_train.py
@@ -17,7 +17,7 @@ rep_buf_s = [450000]
 
 
 def epsi_func(now_episode):
-    return max(0.01, 0.5 * (math.tanh(-0.01 * now_episode + 1.5) + 1))
+    return max(0.01, 0.3 * 0.5 * (math.tanh(-0.01 * now_episode + 1.5) + 1))
 
 
 count = 0
